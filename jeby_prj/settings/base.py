bc9c0f1ac88d5dd88bb74109115033bdb4d5b164
engyjoon <engyjoon@gmail.com>
2021-08-01 03:33:19 +0900

    STATICFILES_DIRS 설정 추가

--- jeby_prj/settings/base.py
+++ jeby_prj/settings/base.py
@@ -141,6 +141,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / '_static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
