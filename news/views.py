e25b3b2236d4c4d7cb2cec9c44675fe2ff976934
engyjoon <engyjoon@gmail.com>
2021-08-03 17:59:31 +0900

    뉴스 검색 기본 화면 개발

--- news/views.py
+++ news/views.py
@@ -1,10 +1,25 @@
+from urllib.parse import quote
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
 @login_required(login_url='common:login')
 def index(request):
+    """뉴스 검색 첫 페이지"""
+
     return render(
         request,
         'news/index.html',
+    )
+
+
+def news_search(request):
+    """키워드로 뉴스를 검색한다."""
+    if request.method == 'GET':
+        keyword = quote(request.GET.get('keyword'))
+        print(keyword)
+
+    return render(
+        request,
+        'news/read.html',
     )
