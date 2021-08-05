4e41adc3c319b1a64b1cb1477ea443f3104aaa3f
engyjoon <engyjoon@gmail.com>
2021-08-05 13:34:02 +0900

    뉴스검색 메뉴 active class 추가

--- jeby_prj/static/news/js/news_index.js
+++ jeby_prj/static/news/js/news_index.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    $('#search-img').click(function () {
-        $('#frm-search').submit();
+    $(".nav-link").eq(0).addClass("active");
+
+    $("#search-img").click(function () {
+        $("#frm-search").submit();
     });
 });
