18ada3c4e3b482e6e920fad29f755756ff44bbc9
engyjoon <engyjoon@gmail.com>
2021-08-05 15:43:11 +0900

    Keyword 모델 생성

--- news/models.py
+++ news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
+from django.db.models.fields import CharField
 
-# Create your models here.
+
+class Keyword(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    content = models.CharField(max_length=100)
+    order = models.IntegerField(
+        validators=[MinValueValidator(1)],
+        unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    shared = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
