90ae79464c05ce6e84e6647533a4308b6c538c1c
engyjoon <engyjoon@gmail.com>
2021-08-05 16:33:34 +0900

    keyword 조회 화면 생성

--- news/models.py
+++ news/models.py
@@ -7,11 +7,15 @@ from django.db.models.fields import CharField
 class Keyword(models.Model):
     title = models.CharField(max_length=50, unique=True)
     content = models.CharField(max_length=100)
+    mailing = models.BooleanField(default=False)
+    shared = models.BooleanField(default=False)
     order = models.IntegerField(
         validators=[MinValueValidator(1)],
         unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    shared = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['order']
