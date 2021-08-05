86b16a5ad46c4f5323537bc67973e601cbba689c
engyjoon <engyjoon@gmail.com>
2021-08-05 13:19:41 +0900

    에러 메시지 출력 기능 추가

--- news/views.py
+++ news/views.py
@@ -18,18 +18,25 @@ def news_search(request):
     """키워드로 뉴스를 검색한다."""
 
     form = {}
+    error_msg = None
+    template_name = 'news/read.html'
 
     news = None
     if request.method == 'POST':
-        keyword = request.POST.get('keyword')
-        news = naverapi.get_news(keyword)
-        form['keyword'] = keyword
+        keyword = request.POST.get('keyword').strip()
+        if keyword is None or keyword == '':
+            error_msg = "키워드를 입력해 주세요."
+            template_name = 'news/index.html'
+        else:
+            news = naverapi.get_news(keyword)
+            form['keyword'] = keyword
 
     return render(
         request,
-        'news/read.html',
+        template_name,
         {
             'news': news,
             'form': form,
+            'error_msg': error_msg,
         }
     )
