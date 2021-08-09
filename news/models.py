06e3da2ac2ead599cc6effbcd5f90daced6a5c49
engyjoon <engyjoon@gmail.com>
2021-08-09 10:58:03 +0900

    Keyword 모델에서 title, author 복합 unique 적용

--- news/models.py
+++ news/models.py
@@ -5,7 +5,7 @@ from django.db.models.fields import CharField
 
 
 class Keyword(models.Model):
-    title = models.CharField(max_length=50, unique=True, verbose_name='관리명')
+    title = models.CharField(max_length=50, verbose_name='관리명')
     content = models.CharField(max_length=100, verbose_name='키워드')
     mailing = models.BooleanField(default=False, verbose_name='메일발송')
     shared = models.BooleanField(default=False, verbose_name='공유')
@@ -19,3 +19,4 @@ class Keyword(models.Model):
 
     class Meta:
         ordering = ['order']
+        unique_together = ('title', 'author',)
