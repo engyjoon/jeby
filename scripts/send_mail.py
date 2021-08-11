e6d90c7e44410b73f7eaf3249a4875c8d2a3c24d
engyjoon <engyjoon@gmail.com>
2021-08-11 11:18:13 +0900

    현재 시간 시간으로 메일을 발송하도록 수정

--- scripts/send_mail.py
+++ scripts/send_mail.py
@@ -1,3 +1,7 @@
+from datetime import datetime
 from news import naverapi
 
-naverapi.send_email_by_schedule('09:00')
+now = datetime.now()
+current_time = str(now.hour).zfill(2) + ':' + str(now.minute).zfill(2)
+
+naverapi.send_email_by_schedule(current_time)
