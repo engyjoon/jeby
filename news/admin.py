351dd5d287a762af318498ca1993052951b22ece
engyjoon <engyjoon@gmail.com>
2021-08-22 23:21:42 +0900

    Site 모델을 Admin 페이지에 등록

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Keyword, Setting
+from .models import Keyword, Setting, Site
 
 
 admin.site.register(Keyword)
 admin.site.register(Setting)
+admin.site.register(Site)
