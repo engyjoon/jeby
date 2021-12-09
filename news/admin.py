7989d49e12669888448cd461a8e991b82e3e2c65
engyjoon <engyjoon@gmail.com>
2021-12-09 21:41:27 +0900

    Keyword 모델 관리페이지 설정 추가

--- news/admin.py
+++ news/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import Keyword, Recipient, Setting, Site
 
 
-admin.site.register(Keyword)
+@admin.register(Keyword)
+class KeywordAdmin(admin.ModelAdmin):
+    list_display = ["title", "content", "mailing", "author"]
+    list_filter = ("author",)
+
+
 admin.site.register(Recipient)
 admin.site.register(Setting)
 admin.site.register(Site)
